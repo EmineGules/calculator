1c84a5285407d82ab0a457e1c301973d1bc45614
eminegules <220502020@st.atlas.edu.tr>
2023-11-15 21:40:46 -0800

    Added a divide() method to Calculator

---
+++ Calculator.java
@@ -0,0 +1,26 @@
+public class Calculator() {
+public static void main(String[] args) {}
+
+public class Calculator {
+    public int addition(int a, int b) {
+        return a + b;
+    }
+
+    public double addition(double a, double b) {
+        return a + b;
+}
+
+public int subtraction(int a, int b) {
+ return a - b;
+ }
+ public double subtraction(double a, double b) {
+ return a - b;
+ }
+ublic int divide(int a, int b) {
+        return a / b;
+    }
+
+    public double divide(double a, double b) {
+        return a / b;
+    }
+}
